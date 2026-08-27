e186a0e9511d4d30cc79145593f36fa80e877b34
Azmiya . <azmiyaazmii25@gmail.com>
2026-08-27 21:48:35 +0530

    Time: 18 ms (77.67%) | Memory: 108.5 MB (23.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+/*          Arrays.sort(nums);
         int i=0;
         int j=1;
-        for(i=0;i<nums.length-1;i++){
+        for(i=0;i<nums.length;i++){
             if(nums[i]!=nums[j]){
                 j=j+1;
             } else
             return true;
 
         }
-        return false;
+        return false; */
+
+        Set<Integer> Set= new HashSet<>();
+        for(int num:nums){
+            if(Set.contains(num)){
+                return true;
+            }
+            Set.add(num);
+        }
+      return false;
     }
 }
